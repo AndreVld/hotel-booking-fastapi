6cbd10ba0c692381b6b050a03d1d8d75c0e0042d
AndreVld <iandre.vld@gmail.com>
2024-02-07 14:27:49 +0300

    rewritten models with typing

--- app/users/models.py
+++ app/users/models.py
@@ -1,10 +1,18 @@
 from sqlalchemy import Column, Integer, String
 from app.database import Base
+from sqlalchemy.orm import Mapped, mapped_column
 
+
+# class Users(Base):
+#     __tablename__ = 'users'
+
+#     id = Column(Integer, primary_key=True)
+#     email = Column(String, nullable=False)
+#     hashed_password = Column(String, nullable=False)
 
 class Users(Base):
     __tablename__ = 'users'
 
-    id = Column(Integer, primary_key=True)
-    email = Column(String, nullable=False)
-    hashed_password = Column(String, nullable=False)
+    id:Mapped[int] = mapped_column(primary_key=True)
+    email:Mapped[str] = mapped_column(nullable=False)
+    hashed_password:Mapped[str] =  mapped_column(nullable=False)
