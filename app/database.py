770bd4a67dd4b5b38046a52824e49ae9185af700
AndreVld <iandre.vld@gmail.com>
2024-03-06 15:48:13 +0300

    fixed bug in database URL

--- app/database.py
+++ app/database.py
@@ -9,7 +9,7 @@ if st.MODE == "TEST":
     DATABASE_URL += f"{st.TEST_DB_USER}:{st.TEST_DB_PASS}@{st.TEST_DB_HOST}:{st.TEST_DB_PORT}/{st.TEST_DB_NAME}"
     DATABASE_PARAMS = {"poolclass": NullPool}
 else:
-    DATABASE_URL = f"{st.DB_USER}:{st.DB_PASS}@{st.DB_HOST}:{st.DB_PORT}/{st.DB_NAME}"
+    DATABASE_URL += f"{st.DB_USER}:{st.DB_PASS}@{st.DB_HOST}:{st.DB_PORT}/{st.DB_NAME}"
     DATABASE_PARAMS = {}
 
 
