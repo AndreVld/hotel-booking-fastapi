3449227ad968a294253801da8b5d767527922dfc
AndreVld <iandre.vld@gmail.com>
2024-03-20 20:32:08 +0300

    сonfigured Nginx

--- docker/celery.sh
+++ docker/celery.sh
@@ -6,5 +6,5 @@ if [[ "${1}" == "celery" ]]; then
 elif [[ "${1}" == "celery_beat" ]]; then
     celery --app=app.tasks.celery_conf:celery_app worker -l INFO -B
 elif [[ "${1}" == "flower" ]]; then
-    celery --app=app.tasks.celery_conf:celery_app flower
+    celery --app=app.tasks.celery_conf:celery_app flower --url_prefix=/flower
 fi
